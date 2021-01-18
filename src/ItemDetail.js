2e43c89fcf4b72c820b58e30c28932f5d265b09e
andrejstrashun <andrejstrashun@gmail.com>
2021-01-18 21:13:46 +0200

    3

---
+++ src/ItemDetail.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function ItemDetail({ match }) {
+  useEffect(() => {
+      fetchItem();
+      console.log(match);
+  }, []);
+
+  const [item, setItem] = useState({
+      images: {}
+  });
+
+  const fetchItem = async() => {
+      const fetchItem = await fetch(
+            'https://fortnite-public-api.theapinetwork.com/prod09/item/get?ids=${match.params.id}'
+      );
+      const item = await fetchItem.json();
+        setItem(item);
+      console.log(item);
+  }
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <img src={item.images.transparent} alt="" />
+      <h1>Item</h1>
+    </div>
+  );
+}
+
+export default ItemDetail;
